18394012805e8861a33485b9d51cf6842018d7f1
DaviMatheus <matdavimat@gmail.com>
2023-11-06 21:52:10 -0300

    🐛 deleting_migrations

--- prisma/migrations/20231106232603_adding_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[nome]` on the table `Unidade_Policia` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Unidade_Policia_nome_key" ON "Unidade_Policia"("nome");
