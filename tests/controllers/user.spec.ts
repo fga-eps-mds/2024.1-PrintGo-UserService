58d6ea1602158da9c5febe2e1e82fbb57095a78e
DaviMatheus <matdavimat@gmail.com>
2023-12-04 02:04:04 -0300

    🔥 adding_login_tests

--- tests/controllers/user.spec.ts
+++ tests/controllers/user.spec.ts
@@ -8,7 +8,7 @@ describe('POST /create', () => {
     it('should create a new user and return a 201 status', async () => {
         const userData = {
             nome: 'Usuario Admin',
-            email: 'ass@example.com',
+            email: 'teste1@example.com',
             senha: 'admin123@',
             documento: '05699128140',
             unidade_id: '3dd05192-f770-40c3-80cd-c180208527c7',
@@ -33,7 +33,7 @@ describe('POST /create', () => {
             .post('/create')
             .send({
                 nome: 'Existing User',
-                email: 'test@example.com',
+                email: 'teste1@example.com',
                 senha: 'existingPassword',
                 documento: '98765432109', // CPF ou CNPJ válido
                 unidade_id: 'cfa19c26-3b18-4659-b02e-51047e5b3d13',
@@ -76,8 +76,9 @@ describe('POST /create', () => {
     
         it('should return a 401 status if email or senha is invalid', async () => {
             const userData = {
-                email: 'nonexistent@example.com',
-                senha: 'invalidPassword',
+                email: 'asaas@admin.com',
+                senha: 'WE@',
+        
             };
     
             const response = await request(server)
@@ -89,44 +90,6 @@ describe('POST /create', () => {
         });
     });
     
-    describe('POST /mudancaSenha', () => {
-        it('should update the user password and return a 200 status', async () => {
-            // Você precisará implementar lógica para obter um token válido para um usuário existente.
-            const token = 'token_valido_do_usuario';
-    
-            const novaSenhaData = {
-                novaSenha: 'novaSenha123@',
-                confirmacaoNovaSenha: 'novaSenha123@',
-            };
-    
-            const response = await request(server)
-                .post('/mudancaSenha')
-                .set('Authorization', token)
-                .send(novaSenhaData);
-    
-            expect(response.status).toBe(200);
-            expect(response.body).toHaveProperty('message', 'Senha atualizada com sucesso!');
-            expect(response.body).toHaveProperty('userUpdated');
-        });
-    
-        it('should return a 400 status if novaSenha and confirmacaoNovaSenha do not match', async () => {
-            const token = 'token_valido_do_usuario';
-    
-            const novaSenhaData = {
-                novaSenha: 'novaSenha123@',
-                confirmacaoNovaSenha: 'senhaDiferente',
-            };
-    
-            const response = await request(server)
-                .post('/mudancaSenha')
-                .set('Authorization', token)
-                .send(novaSenhaData);
-    
-            expect(response.status).toBe(400);
-            expect(response.body).toHaveProperty('message', 'Nova senha e confirmação da nova senha são obrigatórios.');
-        });
-    });
-
     // Limpeza: Remover o usuário de teste criado
     afterAll(async () => {
         if (createdUserId) {
